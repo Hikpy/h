30dd4bcfc34ec9a8f2ef95b12d4a632a3fdc28b9
Kunpeng Hou <785603062@qq.com>
2019-09-20 11:44:41 +0800

    1
    1

--- send_mail.py
+++ send_mail.py
@@ -7,7 +7,7 @@ from email.header import Header
 # 第三方 SMTP 服务  
 mail_host="smtp.qq.com"  #设置服务器  
 mail_user="785603062@qq.com"    #用户名  
-mail_pass="rckphomiwnutbbde"   #口令,QQ邮箱是输入授权码，在qq邮箱设置 里用验证过的手机发送短信获得，不含空格  
+mail_pass=""   #口令,QQ邮箱是输入授权码，在qq邮箱设置 里用验证过的手机发送短信获得，不含空格  
   
   
 sender = '785603062@qq.com'  
