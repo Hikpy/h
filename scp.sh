6bf02e33bc63f750ee64cda77376bb0831ed73de
H <11391380+Hikpy@users.noreply.github.com>
2020-11-02 17:06:33 +0800

    Create scp.sh

---
+++ scp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ip=$1
+port=$2
+pwd=$3
+
+
+/usr/bin/expect << EOF
+set timeout 900
+spawn scp -P $port root@$ip:/etc/v2-ui/v2-ui.db  /etc/v2-ui/v2-ui.db
+expect {
+"*yes/no" { send "yes\r"; exp_continue}
+"*password:" {send "$pwd\r"}
+}
+
+
+interact
+expect eof
+EOF
